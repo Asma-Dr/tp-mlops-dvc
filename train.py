322855d58d7294296237e910aae4ead1431efce6
DRAOUIL Asma <adraouil@jems-group.com>
2025-12-27 09:49:28 +0100

    TP4 - Initial MLOps project with DVC pipeline

---
+++ train.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+import pickle
+
+# Charger le dataset
+df = pd.read_csv("data/iris.csv")
+
+# Séparer features et target
+X = df.drop(columns="target")
+y = df["target"]
+
+# Split train/test
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Entraîner le modèle
+model = RandomForestClassifier(n_estimators=100, random_state=42)
+model.fit(X_train, y_train)
+
+# Sauvegarder le modèle
+with open("model.pkl", "wb") as f:
+    pickle.dump(model, f)
+
+print("Modèle entraîné et sauvegardé dans model.pkl")
